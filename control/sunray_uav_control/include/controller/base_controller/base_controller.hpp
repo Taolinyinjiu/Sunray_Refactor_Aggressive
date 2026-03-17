f26d139ec686509f6980da36a4069b01bf8b8228
Taolinyinjiu <qwer200413@163.com>
2026-03-17 11:25:07 +0800

    docs: add repository hagindex and update controller header notes
    - add hagindex.yml to describe repository entry points, configs, workflows and core components\n- update base_controller.hpp header comments and formatting guards for the base controller declaration

--- control/sunray_uav_control/include/controller/base_controller/base_controller.hpp
+++ control/sunray_uav_control/include/controller/base_controller/base_controller.hpp
@@ -1,14 +1,17 @@
+/* clang-format off */
 /**
  * @file base_controller.hpp
  * @author Taolinyinjiu@Yun-Drone Tech
  * @brief 本文件旨在抽象出一个Sunray项目中控制器所需要遵循的基类控制器，通过对该基类的继承，我们可以得到多种的控制器
+1. 首先我们得到一个基本的问题，控制器需要面临无人机着地-> 无人机离地这一阶段，动力学的突变，
+
  * @version 0.1
  * @date 2026-03-16
  * 
  * @copyright Copyright (c) 2026
  * 
  */
-
+/* clang-format on */
 #pragma once
 
-
+// 测试
